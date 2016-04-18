7be5750ed30852a68288c5e6f54b65576f8defa6
Enrico Anello <enrico.anello@gmail.com>
2016-04-18 16:36:18 +0200

    Update FAOGlobalCaptureDataset2013.R

--- R/FAOGlobalCaptureDataset2013.R
+++ R/FAOGlobalCaptureDataset2013.R
@@ -5,7 +5,7 @@ plotQuantity <- function(start=1900, end=2100, file="FAOGlobalCaptureDataset2013
   myData <- read.csv(file)
   aggr1 <- aggregate(myData$QUANTITY, by=list(YR_ITEM=myData$YR_ITEM), FUN=sum)
   aggr12 <- transform(aggr1, YR_ITEM = as.character(YR_ITEM), Quantity = as.numeric(x))
-  #aggr13 <- filter(aggr12, YR_ITEM >= start, YR_ITEM <= end)
-  m1 <- mPlot(x = "YR_ITEM", y = c("x"), type = "Line", data = aggr12)
+  aggr13 <- filter(aggr12, YR_ITEM >= start, YR_ITEM <= end)
+  m1 <- mPlot(x = "YR_ITEM", y = c("x"), type = "Line", data = aggr13)
   m1$save('output.html', standalone = TRUE)
 }
