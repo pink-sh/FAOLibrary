3e94887078c285037c2ab6adbc8b9858639a8eae
Enrico Anello <enrico.anello@fao.org>
2016-04-18 12:56:23 +0200

    First Commit

---
+++ R/FAOGlobalCaptureDataset2013.R
@@ -0,0 +1,11 @@
+library(rCharts)
+library(dplyr)
+plotQuantity <- function(start=1900, end=2100) {
+  myData <- read.csv("/home/enrico/Work/R/FAOGlobalCaptureDataset2013.csv")
+  aggr1 <- aggregate(myData$QUANTITY, by=list(YR_ITEM=myData$YR_ITEM), FUN=sum)
+  aggr12 <- transform(aggr1, YR_ITEM = as.character(YR_ITEM), Quantity = as.numeric(x))
+  aggr13 <- filter(aggr12, YR_ITEM >= start, YR_ITEM <= end)
+  lines(aggr13)
+  m1 <- mPlot(x = "YR_ITEM", y = c("x"), type = "Line", data = aggr13)
+  m1$save('/home/enrico/Work/R/mychart3.html', standalone = TRUE)
+}
