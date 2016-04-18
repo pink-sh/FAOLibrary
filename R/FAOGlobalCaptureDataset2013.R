cc175e07f5ffc6c935fbdc8e292f1f3fca05c5d1
Enrico Anello <enrico.anello@gmail.com>
2016-04-18 14:13:34 +0200

    Update FAOGlobalCaptureDataset2013.R

--- R/FAOGlobalCaptureDataset2013.R
+++ R/FAOGlobalCaptureDataset2013.R
@@ -3,6 +3,7 @@ library(dplyr)
 plotQuantity <- function(start=1900, end=2100, file="FAOGlobalCaptureDataset2013.csv") {
   print(file)
   myData <- read.csv(file)
+  print(myData)
   aggr1 <- aggregate(myData$QUANTITY, by=list(YR_ITEM=myData$YR_ITEM), FUN=sum)
   aggr12 <- transform(aggr1, YR_ITEM = as.character(YR_ITEM), Quantity = as.numeric(x))
   aggr13 <- filter(aggr12, YR_ITEM >= start, YR_ITEM <= end)
